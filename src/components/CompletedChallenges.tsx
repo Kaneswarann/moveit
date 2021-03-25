b6c13399d552f17bac3824a19c371270b5af7e81
Rebeka <rebekamaisa@gmail.com>
2021-03-25 19:17:08 -0300

    first commit

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/CompletedChallengens.module.css';
+export function CompletedChallengens() {
+    const {challengesCompleted} = useContext(ChallengesContext)
+    return (
+        <div className={styles.completedChallengesContainer}>
+            <span>Desafios completos</span>
+            <span>{challengesCompleted}</span>
+        </div>
+    )
+}
